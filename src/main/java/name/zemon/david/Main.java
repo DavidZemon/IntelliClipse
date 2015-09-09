de876f309ee15212c93113f0f9f49b3e9e0de3ec
David Zemon <david@zemon.name>
2015-09-08 22:44:10 -0500

    Refactor

--- src/main/java/name/zemon/david/Main.java
+++ src/main/java/name/zemon/david/Main.java
@@ -1,11 +1,6 @@
 package name.zemon.david;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-
     public String run() {
         return "Hello";
     }
